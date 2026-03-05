481195ff7313454e09d4d2e47a906a4f334916e9
Arpit Uppal <someone040807@gmail.com>
2026-03-04 23:28:34 -0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ucla-blue': '#2774AE',
+        'ucla-gold': '#FFD100',
+      },
+    },
+  },
+  plugins: [],
+};
