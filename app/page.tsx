183a4faf918d97a6d4abf836ec1b8f0d912e3037
SigmaCarrytheFlame67 <rscai@g.ucla.edu>
2026-03-13 03:14:26 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function HomePage() {
         <MathDeco latex="v_p(x^n - y^n) = v_p(x-y) + v_p(n)" className="top-10 left-[10%] text-[2.0em]" />
         <MathDeco latex="\displaystyle \sum_{n\geq0} p(n)x^n = \prod_{k\geq1}\frac{1}{1-x^k}" className="top-8 right-[8%] text-[2.4rem]" />
         <MathDeco latex="\displaystyle f\left( \frac{\sum x_i}{n} \right) \leq \frac{\sum f(x_i)}{n}" className="bottom-12 left-[6%] text-[2.5rem]" />
-        <MathDeco latex="\displaystyle d^2 = -a^2\Delta y \Delta z - b^2\Delta x \Delta z - c^2\Delta x \Delta y" className="top-1/3 left-[1%] text-[1.4rem]" />
+        <MathDeco latex="\displaystyle d^2 = -a^2\Delta y \Delta z - b^2\Delta x \Delta z - c^2\Delta x \Delta y" className="top-17 left-[1%] text-[1.4rem]" />
         <MathDeco latex="\displaystyle x^n - 1 = \prod_{d|n} \Phi_d(x)" className="top-1/2 left-[4%] text-[2.3rem]" />
         <MathDeco latex="\displaystyle E\left[\sum_{i=1}^n X_i\right] = \sum_{i=1}^n E[X_i]" className="top-1/2 right-[5%] text-[1.8rem]" />
         <MathDeco latex="\displaystyle |X/G| = \frac{1}{|G|} \sum_{g \in G} |X^g|" className="bottom-16 right-[8%] text-[1.8rem]" />
