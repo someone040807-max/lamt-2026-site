2ee15c600b00a0436fb7258bfd779f16269a342f
SigmaCarrytheFlame67 <rscai@g.ucla.edu>
2026-03-22 19:51:09 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function HomePage() {
         <MathDeco latex="\displaystyle d^2 = -a^2\Delta y \Delta z - b^2\Delta x \Delta z - c^2\Delta x \Delta y" className="hidden md:block absolute top-1/4 left-[2%] text-[1.8rem]" />
         <MathDeco latex="\displaystyle \phi(n) = \sum_{d \mid n} \mu(d) \frac{n}{d}" className="top-1/3 right-[10%] text-[2.0rem]" />
         <MathDeco latex="\displaystyle x^n - 1 = \prod_{d|n} \Phi_d(x)" className="hidden md:block absolute top-1/2 left-[4%] text-[2.3rem]" />
-        <MathDeco latex="\displaystyle E\left[\sum_{i=1}^n X_i\right] = \sum_{i=1}^n E[X_i]" className="top-1/2 right-[5%] text-[1.8rem]" />
+        <MathDeco latex="\displaystyle E\left[\sum_{i=1}^n X_i\right] = \sum_{i=1}^n E[X_i]" className="hidden md:block absolute top-1/2 right-[5%] text-[1.8rem]" />
         <MathDeco latex="\displaystyle |X/G| = \frac{1}{|G|} \sum_{g \in G} |X^g|" className="bottom-16 right-[8%] text-[1.8rem]" />
         
 
