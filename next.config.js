65dd619daaadadadae441aa57e1076dce5957c47
Arpit Uppal <someone040807@gmail.com>
2026-03-23 22:24:33 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/lamt.github.io',
+  assetPrefix: '/lamt.github.io/',
 };
 
 module.exports = nextConfig;
